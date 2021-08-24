f97b85e6749e66a26b3705da009d0e6691ec5229
reve23 <reve172020@gmail.com>
2021-08-24 14:01:22 +0600

    gambling. maybe it will break my entire app

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jv=@vx428scoy^6dyt-j_!t274ylfm3ez3jouh&+zd(3v=!xi+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','dev-poll-app.herokuapp.com']
 
 
 # Application definition
